5c9eef4383e1a2422cc182b9787d6caf43e65a5d
Matheus Eli <matheuselisouzaferreira@hotmail.com>
2022-04-18 13:39:11 -0300

    search debounceTime

--- src/app/cards/cards-list/cards-list.component.ts
+++ src/app/cards/cards-list/cards-list.component.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { debounceTime, distinctUntilChanged, filter, fromEvent, map, Observable } from "rxjs";
 import { Country } from "../card";
 import { CardsService } from "../cards.service";
 
@@ -7,23 +7,33 @@ import { CardsService } from "../cards.service";
     selector: 'app-cards-list',
     templateUrl: './cards-list.component.html'
 })
-export class CardsListComponent {
+export class CardsListComponent implements OnInit{
 
+    @ViewChild('countrySearchInput', { static: true }) countrySearchInput: ElementRef;
     countries$: Observable<Country[]>;
 
-    constructor(private cardService: CardsService) {
+    constructor(private cardService: CardsService) {}
+
+    ngOnInit(): void {
         this.fetchCountries();
+
+        fromEvent(this.countrySearchInput.nativeElement, 'keyup').pipe(
+            map((event: any) => {
+                return event.target.value
+            }),
+            filter((res) => res.length > 2),
+            debounceTime(1000),
+            distinctUntilChanged()
+        ).subscribe((text: string) => {
+            this.onSearchCountry(text)
+        }, (err) => console.log(err));
     }
 
     fetchCountries() {
         this.countries$ = this.cardService.fetchAllCountries();
     }
 
-    onSearchCountry(event) {
-        if(event.target.value != ''){
-                this.countries$ = this.cardService.fetchCountryByName(event.target.value);
-        }else{
-            this.countries$ = this.cardService.fetchAllCountries();
-        }
+    onSearchCountry(text) {
+        this.countries$ = this.cardService.fetchCountryByName(text);
     }
 }
